8b0ed9d2521252af63025e720d3d9739380b7014
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2024-12-24 18:58:16 +0530

    Updated Env variables

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = '5997431446c673a426deb387e61cf6e0'
+const API_KEY = import.meta.env.VITE_API_KEY
 const BASE_URL = 'https://api.themoviedb.org/3'
 
 export const getPopularMovies = async () => {
